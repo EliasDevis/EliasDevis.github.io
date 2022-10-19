8ca97ae6430d3b2d321233680be15d41b390f798
EliasDevis <60856010+EliasDevis@users.noreply.github.com>
2022-10-20 00:11:53 +0200

    Small sass and locale fix

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # My Portfolio #
-Showcase of my projects
+Written in vanilla js
+PS. I'm sure there is a lot of bugs, so pull requests are welcome
 
 ## TODO ##
 - [ ] Page contacts
+- [ ] Blog maybe
 - [ ] Animation
+- [ ] Use bundler
+- [ ] Add gh action to compile sass to css
+- [ ] Delete unused code
 - [ ] Make public
--- js/blocks/about/About.js
+++ js/blocks/about/About.js
@@ -1,13 +1,13 @@
 export default (t) => {
     return /*html*/`
         <section class="about">
+            <div class="about__illustrations">
+                <img src="/images/about-me.png" alt="" class="about__image">
+            </div>
             <div class="about__text">
                 ${t.description.map(text => /*html*/`
                     <p class="about__description">${text}</p>
                 `).join("")}
-            </div>
-            <div class="about__illustrations">
-                <img src="/images/about-me.png" alt="" class="about__image">
             </div>
         </section>
     `
